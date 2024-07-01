d919205f66e6e21f8b1c0e32232b49693ee8fb21
rezwan <rezwan@Xubuntu.myguest.virtualbox.org>
2024-07-01 02:05:54 -0400

    Add reset button

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const INITIAL_GRID_SIZE = 16;
 let gridSize = INITIAL_GRID_SIZE;
+let gridSquares = [];
 
-const button = document.querySelector("button");
-button.addEventListener("click", handleButtonClick)
+//----------------Grid Size Button---------------------
+const gridSizeButton = document.querySelector(".grid-size");
+gridSizeButton.addEventListener("click", changeSize)
 
 // Allow user to chagne the size of the grid
-function handleButtonClick() {
+function changeSize() {
     gridSize = +prompt("Enter grid size (max 100):", INITIAL_GRID_SIZE);
     if (gridSize > 100 || gridSize <= 10)
         gridSize = INITIAL_GRID_SIZE;
@@ -13,12 +15,17 @@ function handleButtonClick() {
     generateGrid();
 }
 
+//----------------Reset Button---------------------
+const resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", resetGrid);
+function resetGrid() {
+    gridSquares.forEach(div => div.classList.remove("color"))
+}
 
-const container = document.querySelector("div");
+
+const container = document.body.lastElementChild;
 container.classList.add("container");
 
-
-let gridSquares = [];
 function generateGrid() {
     let totalGridSquares = gridSize * gridSize;
     for (let i = 0; i < totalGridSquares; ++i) {
@@ -27,8 +34,7 @@ function generateGrid() {
         div.style.width = `calc((100% / ${gridSize}) - (${gridSize - 1}px  / ${gridSize}))`;
         // Change square color on hover, like a pen
         div.addEventListener("mouseenter", function () {
-            div.style.backgroundColor = "blue";
-            // div.classList.add("color")
+            div.classList.add("color");
         });
         gridSquares.push(div);
         container.appendChild(div);
